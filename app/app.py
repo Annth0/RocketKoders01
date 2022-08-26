00fc99dd4cf853dd55f8705033ebb31df866f89f
Anthonne <luangapa@gmail.com>
2022-08-26 18:50:04 -0500

    HTTP query string

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 # Se debe tener instalado el Flask en el terminal
 # pip install Flask
 
-from flask import Flask, render_template
+from re import A
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,8 +10,6 @@ app = Flask(__name__)
 def index():
     return "Rockert Koders 1"
 """
-
-
 
 
 def index():
@@ -24,9 +23,11 @@ def index():
     }
     return render_template('index.html', data=data)
 
+
 @app.route('/holamundo')
 def hola_mundo():
     return "Hola Mundo!"
+
 
 @app.route('/contacto')
 def contacto():
@@ -36,41 +37,59 @@ def contacto():
     }
     return render_template('contacto.html', data=data)
 
-#___________________________________
-# comportamiento dinamico 
+# ___________________________________
+# comportamiento dinamico
 
-#@app.route('/saludo')
-#def saludo():
+# @app.route('/saludo')
+# def saludo():
 #    return 'Hola Koder!'  | normaal
 # Parametro que forma parte de la ruta
+
+
 @app.route('/saludo/<nombre>')
 def saludo(nombre):
     return 'Hola, {0}!'.format(nombre)
-#------------------------------------
+# ------------------------------------
+
 
 @app.route('/suma/<int:valor1>/<int:valor2>')
 def suma(valor1, valor2):
-    #error de tipo porque no puede resultar solo entero
+    # error de tipo porque no puede resultar solo entero
     #  return valor1 + valor2 _________solución abajo
     return 'la suma es: {0}!'.format((valor1 + valor2))
-#------------------------------------
-#Combinar parametros desde ruta
+# ------------------------------------
+# Combinar parametros desde ruta
+
+
 @app.route('/perfil/<nombre>/<int:edad>')
 def perfil(nombre, edad):
     return 'Hola, tu nombre es {0}, y tu edad es {1}!. Bienvenid@'.format(nombre, edad)
-#------------------------------------
+# ------------------------------------
 # Mostrar distindos datos en las plantillas
+
 
 @app.route('/lenguajes')
 def lenguajes():
-    data={
+    data = {
         'hay_lenguajes': True,
-        'lenguajes':['PHP', 'Python', 'Kotlin', 'Java', 'C#', 'JavaScript']
+        'lenguajes': ['PHP', 'Python', 'Kotlin', 'Java', 'C#', 'JavaScript']
     }
     return render_template('lenguajes.html', data=data)
-#___________________________________
+# ___________________________________
 
-# HTTP: 
+# HTTP: Hypertext Transfer Protocol
+# GET, POST, PUT, DELETE.
+
+
+@app.route('/datos')
+def datos():
+    valor1 = request.args.get('valor1')
+# impresion en consola de los elementos de la petición
+    # print(request.args)
+    # en navegador va: http://127.0.0.1:5005/datos?valor1=pepe en donde después del signo ? se añade lo que estamos obteniendo para mostrar en la página
+    '''obtener el valor que contenga el arggumento con llave valor1'''
+    return 'Estos son los datos: {0}'.format(valor1)
+
 
 # se implementam reglas de flask para que el entorno se actualice automaticamente y no se tenga que cerrar y abrir a cada momento el servidor
 # el app.run(debug=True, port=5005) indica que habilite el modo debug (automaticos cambios) y podemos establecer un puerto personalizado
