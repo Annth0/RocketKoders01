b6876bf1ee24cb541dd16a040a48396b27d8cd8e
Anthonne <luangapa@gmail.com>
2022-09-13 19:34:52 -0500

    prueba para página de yenni

--- appdraft/manage.py
+++ appdraft/manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, Server
-from app import inicializar_app
-from config import config
+from appdraft import inicializar_app
+from appdraft.config import config
 from flask_script._compat import text_type
 
 configuracion=config["development"]
